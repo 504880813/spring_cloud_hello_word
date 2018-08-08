abe8fc696409b8a84fd26ab9f8bba53784a01939
bayMax <504880813@qq.com>
2018-08-08 15:17:00 +0800

    init

--- netty-server-client/src/main/java/org/baymax/boot/redis/CusRedisDecoder.java
+++ netty-server-client/src/main/java/org/baymax/boot/redis/CusRedisDecoder.java
@@ -7,10 +7,31 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 
 public class CusRedisDecoder extends ByteToMessageDecoder {
+    private static final byte DOLLAR_BYTE   = '$';
+    private static final byte ASTERISK_BYTE = '*';
+    private static final byte PLUS_BYTE     = '+';
+    private static final byte MINUS_BYTE    = '-';
+    private static final byte COLON_BYTE    = ':';
+
+
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-        //byte 数组解码  TODO 
+//        byte b = in.readByte();
+//        if (b == MINUS_BYTE) {
+//            parseError(in);
+//        } else if (b == ASTERISK_BYTE) {
+//            return parseMultiBulkReply(in);
+//        } else if (b == COLON_BYTE) {
+//            return parseInteger(in);
+//        } else if (b == DOLLAR_BYTE) {
+//            return parseBulkReply(in);
+//        } else if (b == PLUS_BYTE) {
+//            return parseStatusCodeReply(in);
+//        } else {
+//            throw new Exception("Unknown reply: " + (char) b);
+//        }
+//        //byte 数组解码  TODO 
         byte[] byteArray = new byte[in.readableBytes()];
         in.readBytes(byteArray);
         String result = new String(byteArray);
@@ -36,6 +57,34 @@ public class CusRedisDecoder extends ByteToMessageDecoder {
             }
 
         }
+    }
+
+
+
+    private static String readLine(ByteBuf byteBuf) {
+        int b;
+        byte c;
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            b = byteBuf.readByte();
+            if (b == '\r') {
+
+                c = byteBuf.readByte();
+                if (c == '\n') {
+                    break;
+                }
+                sb.append((char) b);
+                sb.append((char) c);
+            } else {
+                sb.append((char) b);
+            }
+        }
+        String reply = sb.toString();
+        if (reply.length() == 0) {
+//            throw new Exception("It seems like server has closed the connection.");
+        }
+        return reply;
     }
 
 }
